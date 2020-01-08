0b88caeaf2e3051d331d2788e09b05c1477dd5ce
Elisha Johnson <elishamagicjohnson@gmail.com>
2020-01-07 18:22:41 -0600

    chapter 13 studio

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,8 +1,20 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank(message = "Field is required.")
+    @Size(min = 5, max = 15, message = "Must be between 5 and 15 characters.")
     private String username;
+
+    @Email(message = "Must be a valid Email address.")
     private String email;
+
+    @NotBlank(message = "Field is required.")
+    @Size(min = 6, message = "Must be at least 6 characters long.")
     private String password;
 
     public User() {
